081898b326d9423245150bc78d5547d6a8755fb2
seyfhad <seyfhad@gmail.com>
2026-09-21 11:38:15 +0100

    Change base path in Vite config to '/MotoDrive/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/MotoDrive/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
