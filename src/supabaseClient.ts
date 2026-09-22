8a04f5cb0bd2cc50363b84783571ca5a3ab05b28
seyfhad <seyfhad@gmail.com>
2026-09-22 19:58:35 +0100

    Update Supabase client URL and key

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'رابط_المشروع_هنا';
-const supabaseKey = 'المفتاح_العام_هنا';
+const supabaseUrl = 'https://uvevnwjjcprsykikaweb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2ZXZud2pqY3Byc3lraWthd2ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwOTc2NDcsImV4cCI6MjEwNTY3MzY0N30.mOOcezseKuN6Ww-Rs29yIzvL9AJNoNATfbU7zEq2rU4';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
